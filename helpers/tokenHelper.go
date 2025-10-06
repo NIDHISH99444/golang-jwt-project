34f02d9a30e9f37734c626c35aab04c053be9dec
Nidhish <nidhish99444@gmail.com>
2025-10-06 22:32:43 +0530

    adding new changes

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,13 +1,18 @@
 package helpers
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/nidhish/golang-jwt-project/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -59,4 +64,55 @@ func GenerateAllTokens(email string, firstNme string, lastName string, userType 
 
 	return token, refreshToken, err
 
+}
+
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userId string) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	var updateObj primitive.D
+
+	updateObj = append(updateObj, bson.E{"token", signedToken})
+	updateObj = append(updateObj, bson.E{"token", signedRefreshToken})
+	updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, bson.E{"updated_at", updated_at})
+	upsert := true
+	filter := bson.M{"user_id": userId}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+
+	_, err := userCollection.UpdateOne(ctx, filter, bson.D{{"$set", updateObj}}, &opt)
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+	return
+}
+
+func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetails{},
+		func(t *jwt.Token) (any, error) {
+			if t.Method != jwt.SigningMethodHS256 {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
+			if SECRET_KEY == "" {
+				return nil, fmt.Errorf("SECRET_KEY not set")
+			}
+			return []byte(SECRET_KEY), nil
+		},
+	)
+	if err != nil {
+		return nil, err.Error()
+	}
+
+	claims, ok := token.Claims.(*SignedDetails)
+
+	if !ok || !token.Valid {
+		return nil, "invalid token"
+	}
+
+	return claims, ""
 }
